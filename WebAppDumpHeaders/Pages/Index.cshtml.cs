8149c80a4a96f3b65f649ff0f642bee05833b77a
krishsub <krishsub@DESKTOP-T8VKF2U>
2023-09-29 10:44:51 +0200

    Add project files.

---
+++ WebAppDumpHeaders/Pages/Index.cshtml.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebAppDumpHeaders.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+            HttpContext.Response.Headers.Add("HYPHEN-TEST", "TEST-VALUE");
+            HttpContext.Response.Headers.Add("UNDERSCORE_TEST", "TEST_VALUE");
+        }
+    }
+}
