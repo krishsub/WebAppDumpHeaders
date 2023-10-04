b9e163d517bde40d1ecf9cfd262dff02336a2d57
Krishnan Subramanian <krishsub@microsoft.com>
2023-10-04 15:12:37 +0200

    Cookies added

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # WebAppDumpHeaders
+A simple web app that dumps the headers of the incoming request and response from the application.
+
+Useful for diagnosing issues with reverse proxies.
--- WebAppDumpHeaders/Pages/Index.cshtml.cs
+++ WebAppDumpHeaders/Pages/Index.cshtml.cs
@@ -5,15 +5,33 @@ namespace WebAppDumpHeaders.Pages
 {
     public class IndexModel : PageModel
     {
+        public int PageViewCount { get; set; }
         private readonly ILogger<IndexModel> _logger;
 
         public IndexModel(ILogger<IndexModel> logger)
         {
             _logger = logger;
+            PageViewCount = 1;
         }
 
         public void OnGet()
         {
+            var pageViewCountCookie = Request.Cookies["PageViewCount"];
+            CookieOptions option = new()
+            {
+                Expires = DateTime.Now.AddYears(1)
+            };
+            if (pageViewCountCookie == null)
+            {
+
+                Response.Cookies.Append("PageViewCount", "1", option);
+            }
+            else
+            {
+                PageViewCount = int.Parse(pageViewCountCookie) + 1;
+                Response.Cookies.Append("PageViewCount", PageViewCount.ToString(), option);
+            }
+
             HttpContext.Response.Headers.Add("HYPHEN-TEST", "TEST-VALUE");
             HttpContext.Response.Headers.Add("UNDERSCORE_TEST", "TEST_VALUE");
         }
